bbb8c514faa79639d99b5f24dd869050f358e224
Markus <markus@hellocash.at>
2021-07-23 09:02:53 +0200

    height fix

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <main class="dark:bg-gray-600 bg-gray-200 dark:text-white h-screen text-lg">
+  <main
+    class="h-full dark:bg-gray-600 bg-gray-200 dark:text-white h-screen text-lg"
+  >
     <Toasts />
     <router-view v-slot="{ Component, route }">
       <transition appear :name="route.meta.transition || transitionName">
         <keep-alive include="Overview">
           <component
             :is="Component"
-            class="flex flex-col dark:bg-gray-600 h-screen bg-gray-200"
+            class="h-full flex flex-col dark:bg-gray-600 h-screen bg-gray-200"
           />
         </keep-alive>
       </transition>
