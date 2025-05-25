70412384e2348ce4a6c59f9e03f5d6a3c1f2f583
MarkusJLechner <markus@hellocash.at>
2025-05-25 19:41:27 +0200

    Configure Vitest with JSDOM, Vue support, and path aliases.
    Switches the test environment to JSDOM in the Vitest setup and adds Vue plugin support. Implements custom path aliases for project directories and adjusts dependencies accordingly, including adding `jsdom`. Updates Prettier configuration to align with modern JavaScript export syntax.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const resolve = (dir) => {
+  return path.resolve(dirname, dir)
+}
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
-    environment: 'node',
+    environment: 'jsdom',
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+      pages: resolve('./src/pages'),
+      components: resolve('./src/components'),
+      plugins: resolve('./src/plugins'),
+      router: resolve('./src/router'),
+      store: resolve('./src/store'),
+      assets: resolve('./src/assets'),
+    },
   },
 })
