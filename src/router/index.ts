82ad181335579411d1a8a586f3a460fe23cee533
MarkusJLechner <markus@hellocash.at>
2025-05-29 20:32:44 +0200

    chore(router/types): refactor route components to lazy loading and add Vue type declarations
    - Updated route components in `router/index.ts` to use dynamic imports for improved performance.
    - Added `shims-vue.d.ts` to provide Vue type definitions and resolve module issues.

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,118 +12,87 @@ if (!import.meta.hot) {
   }
 }
 
-import Overview from 'pages/Overview.vue'
-import Browser from 'pages/Browser.vue'
-import Help from 'pages/Help.vue'
-import Support from 'pages/Support.vue'
-import Statistics from 'pages/Statistics.vue'
-import Review from 'pages/Review.vue'
-import CardInfo from 'pages/CardInfo.vue'
-import DeckOptions from 'pages/DeckOptions.vue'
-import Error from 'pages/Error.vue'
-import Settings from 'pages/Settings.vue'
-import GeneralSettings from 'pages/Settings/GeneralSettings.vue'
-import Advanced from 'pages/Settings/Advanced.vue'
-import Appearance from 'pages/Settings/Appearance.vue'
-import Gestures from 'pages/Settings/Gestures.vue'
-import Reviewing from 'pages/Settings/Reviewing.vue'
-import Shared from '@/pages/Shared.vue'
-
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Overview',
-    component: Overview,
-    // component: () => import('pages/Overview.vue'),
+    component: () => import('pages/Overview.vue'),
   },
   {
     path: '/browser',
     name: 'Browser',
-    component: Browser,
-    // component: () => import('pages/Browser.vue'),
+    component: () => import('pages/Browser.vue'),
   },
   {
     path: '/help',
     name: 'Help',
-    component: Help,
-    // component: () => import('pages/Help.vue'),
+    component: () => import('pages/Help.vue'),
   },
   {
     path: '/support',
     name: 'Support',
-    component: Support,
-    // component: () => import('pages/Support.vue'),
+    component: () => import('pages/Support.vue'),
   },
   {
     path: '/shared',
     name: 'shared',
-    component: Shared,
-    // component: () => import('pages/Support.vue'),
+    component: () => import('@/pages/Shared.vue'),
   },
   {
     path: '/statistics',
     name: 'Statistics',
-    component: Statistics,
-    // component: () => import('pages/Statistics.vue'),
+    component: () => import('pages/Statistics.vue'),
   },
   {
     path: '/review/on',
     name: 'Review',
-    component: Review,
-    // component: () => import('pages/Review.vue'),
+    component: () => import('pages/Review.vue'),
   },
   {
     path: '/deck/options',
     name: 'DeckOptions',
-    component: DeckOptions,
+    component: () => import('pages/DeckOptions.vue'),
   },
   {
     path: '/card/info',
     name: 'CardInfo',
-    component: CardInfo,
+    component: () => import('pages/CardInfo.vue'),
   },
   {
     path: '/:catchAll(.*)',
-    component: Error,
-    // component: () => import('pages/Error.vue'),
+    component: () => import('pages/Error.vue'),
   },
 
   {
     path: '/settings/overview',
     name: 'Settings',
-    component: Settings,
-    // component: () => import('pages/Settings.vue'),
+    component: () => import('pages/Settings.vue'),
   },
 
   {
     path: '/setting/wanki/general',
     name: 'GeneralSettings',
-    component: GeneralSettings,
-    // component: () => import('pages/Settings/GeneralSettings.vue'),
+    component: () => import('pages/Settings/GeneralSettings.vue'),
   },
   {
     path: '/setting/wanki/advanced',
     name: 'Advanced',
-    component: Advanced,
-    // component: () => import('pages/Settings/Advanced.vue'),
+    component: () => import('pages/Settings/Advanced.vue'),
   },
   {
     path: '/setting/wanki/appearance',
     name: 'Appearance',
-    component: Appearance,
-    // component: () => import('pages/Settings/Appearance.vue'),
+    component: () => import('pages/Settings/Appearance.vue'),
   },
   {
     path: '/setting/wanki/gestures',
     name: 'Gestures',
-    component: Gestures,
-    // component: () => import('pages/Settings/Gestures.vue'),
+    component: () => import('pages/Settings/Gestures.vue'),
   },
   {
     path: '/setting/wanki/reviewing',
     name: 'Reviewing',
-    component: Reviewing,
-    // component: () => import('pages/Settings/Reviewing.vue'),
+    component: () => import('pages/Settings/Reviewing.vue'),
   },
 ]
 
@@ -131,8 +100,8 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
   scrollBehavior(
-    to: RouteLocationNormalized,
-    from: RouteLocationNormalized,
+    _to: RouteLocationNormalized,
+    _from: RouteLocationNormalized,
     savedPosition: Parameters<RouterScrollBehavior>[2],
   ) {
     if (savedPosition) {
